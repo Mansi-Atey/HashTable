7072837d6de3a348583a432379292336f568fc0b
Mansi-Atey <mansiatey31@gmail.com>
2021-03-26 16:44:41 +0530

    [mansi]ADDED: code to find frequency of words in paragraph

---
+++ UC2-HashTable/UC2-HashTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UC2_HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] para = paragraph.Split(" ");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(para.Length);
+            int key = 0;
+            foreach (string word in para)
+            {
+                hash.Add(key, word);
+                key++;
+            }
+            Operation operation = new Operation();
+            operation.Frequency(hash);
+        }
+    }
+}
